d4067361965a7f889732c7a887755cf3adf9e1d0
mubashir-shaikh-code <mubashiraijaz1@gmail.com>
2025-09-18 15:24:19 +0500

    update project

---
+++ prisma/migrations/20250918101809_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - The values [APPROVED] on the enum `EventRequest_status` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE `eventrequest` MODIFY `status` ENUM('PENDING', 'ACCEPTED', 'REJECTED') NOT NULL DEFAULT 'PENDING';
